8f7c0b45bd61307c0b5d55f1ee4881d41d786a54
Kaede Sato <main@kaedesato.work>
2025-05-07 15:06:49 +0000

    右クリック有効化

--- keyboards/stront/rules.mk
+++ keyboards/stront/rules.mk
@@ -5,8 +5,8 @@ POINTING_DEVICE_DRIVER = cirque_pinnacle_spi
 
 QUANTUM_PAINTER_LVGL_INTEGRATION = yes
 # choose one display driver:
-QUANTUM_PAINTER_DRIVERS = st7789_spi # rectangular display
-# QUANTUM_PAINTER_DRIVERS = gc9a01_spi # round display
+# QUANTUM_PAINTER_DRIVERS = st7789_spi # rectangular display
+QUANTUM_PAINTER_DRIVERS = gc9a01_spi # round display
 
 SRC += lvgl_helpers.c \
 	   display.c
